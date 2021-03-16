ef99f3f682cdd4f070110a21b3b4fa60326a0900
Juan Silva <silvajua@nro.mil>
2021-03-16 14:32:49 +0000

    refactor initController to one endpoint

--- src/main/java/mil/af/abms/midas/api/init/InitController.java
+++ src/main/java/mil/af/abms/midas/api/init/InitController.java
@@ -9,14 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 import mil.af.abms.midas.api.announcement.Announcement;
 import mil.af.abms.midas.api.announcement.AnnouncementService;
 import mil.af.abms.midas.api.announcement.dto.AnnouncementDTO;
-import mil.af.abms.midas.api.init.dto.InfoDTO;
+import mil.af.abms.midas.api.init.dto.InitDTO;
 import mil.af.abms.midas.api.user.UserService;
 import mil.af.abms.midas.api.user.dto.UserDTO;
 import mil.af.abms.midas.config.CustomProperty;
@@ -38,25 +37,12 @@ public class InitController {
     }
 
     @ApiOperation(value = "context info",
-            notes = "Returns classification context, as well as available roles a user may have")
-    @GetMapping("/info")
-    public InfoDTO getInfo() {
-        return new InfoDTO(property.getClassification(), property.getCaveat());
-    }
-
-    @ApiOperation(value = "Returns currently logged in user",
-            notes = "PlatformOne identity management never informs the frontend of the user identity")
-    @GetMapping("/user")
-    public UserDTO loginInit(Authentication auth) throws JsonProcessingException {
-        return userService.getUserFromAuth(auth).toDto();
-    }
-
-    @ApiOperation(value = "returns unseen announcements",
-            notes = "Looks for unseen announcements since user last login")
-    @GetMapping("/announcements")
-    public List<AnnouncementDTO> getUnseenAnnouncement(Authentication auth) {
-
-        return announcementService.getUnseenAnnouncements(userService.getUserFromAuth(auth))
+            notes = "Returns classification context, available roles a user may have, user login, and unseen announcements")
+    @GetMapping
+    public InitDTO getInfo(Authentication auth) {
+        UserDTO userDTO = userService.getUserFromAuth(auth).toDto();
+        List<AnnouncementDTO> announcementDTOs = announcementService.getUnseenAnnouncements(userService.getUserFromAuth(auth))
                 .stream().map(Announcement::toDto).collect(Collectors.toList());
+        return new InitDTO(property.getClassification(), property.getCaveat(), userDTO, announcementDTOs);
     }
 }
--- src/test/java/mil/af/abms/midas/api/init/InitControllerTest.java
+++ src/test/java/mil/af/abms/midas/api/init/InitControllerTest.java
@@ -3,9 +3,7 @@ package mil.af.abms.midas.api.init;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,7 +11,6 @@ import java.util.Optional;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -39,24 +36,18 @@ public class InitControllerTest extends ControllerTestHarness {
     }
 
     @Test
-    public void should_get_public_info() throws Exception {
-        String endpoint = "/init/info";
-
-        when(property.getClassification()).thenReturn("UNCLASS");
-        when(property.getCaveat()).thenReturn("IL2");
-
-        mockMvc.perform(get(endpoint))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(jsonPath("$.classification").isMap());
-    }
-
-    @Test
     public void should_get_from_public_info() throws Exception {
+        Announcement announcementEntity = Builder.build(Announcement.class)
+                .with(a -> a.setId(1L))
+                .with(a -> a.setCreationDate(LocalDateTime.now()))
+                .with(a -> a.setMessage("HELLO")).get();
+
+        when(userService.getUserFromAuth(any())).thenReturn(authUser);
+        when(announcementService.getUnseenAnnouncements(any())).thenReturn(List.of(announcementEntity));
         when(property.getClassification()).thenReturn("UNCLASS");
         when(property.getCaveat()).thenReturn("IL2");
 
-        mockMvc.perform(get("/init/info"))
+        mockMvc.perform(get("/init"))
                 .andExpect(jsonPath("$.classification").isMap())
                 .andExpect(jsonPath("$.classification.name").isNotEmpty())
                 .andExpect(jsonPath("$.classification.caveat").isNotEmpty())
@@ -65,30 +56,10 @@ public class InitControllerTest extends ControllerTestHarness {
                 .andExpect(jsonPath("$.roles").isArray())
                 .andExpect(jsonPath("$.roles[*].name").isNotEmpty())
                 .andExpect(jsonPath("$.roles[*].offset").isNotEmpty())
-                .andExpect(jsonPath("$.roles[*].description").isNotEmpty());
-    }
-
-    @Test
-    public void should_login_locally() throws Exception {
-        mockMvc.perform(get("/init/user"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(jsonPath("$.keycloakUid").value("abc-123"));
-    }
-    
-    @Test
-    public void should_return_unseen_comments() throws Exception {
-        Announcement announcementEntity = Builder.build(Announcement.class)
-                .with(a -> a.setId(1L))
-                .with(a -> a.setCreationDate(LocalDateTime.now()))
-                .with(a -> a.setMessage("HELLO")).get();
-
-        when(userService.getUserFromAuth(any())).thenReturn(authUser);
-        when(announcementService.getUnseenAnnouncements(any())).thenReturn(List.of(announcementEntity));
-
-        mockMvc.perform(get("/init/announcements"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(jsonPath("$[0].message").value("HELLO"));
+                .andExpect(jsonPath("$.roles[*].description").isNotEmpty())
+                .andExpect(jsonPath("$.userDTO").isNotEmpty())
+                .andExpect(jsonPath("$.userDTO.keycloakUid").value("abc-123"))
+                .andExpect(jsonPath("$.announcementDTOs[0].message").value("HELLO"))
+                .andExpect(jsonPath("$.announcementDTOs").isArray());
     }
 }
