853dead1e58d3089226bceebeae0bc07b1931d9e
Jeffrey Wills <jwills@rise8.us>
2021-03-18 14:35:55 -0400

    added constraints to DTO

--- src/main/java/mil/af/abms/midas/api/product/dto/CreateProductDTO.java
+++ src/main/java/mil/af/abms/midas/api/product/dto/CreateProductDTO.java
@@ -1,15 +1,20 @@
 package mil.af.abms.midas.api.product.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
 import mil.af.abms.midas.api.product.validation.UniqueName;
 
 @AllArgsConstructor
 @Getter
 public class CreateProductDTO {
+
+    @NotBlank
     @UniqueName(isNew = true)
     String name;
-    String description;
+    @NotNull
     Long gitlabProjectId;
+    String description;
 }
