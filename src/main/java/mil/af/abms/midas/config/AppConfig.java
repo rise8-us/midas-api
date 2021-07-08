a378fbcaed1fe7070db38afe69b0bb41c3b24d90
Peter Duong <pduong@rise8.us>
2021-07-08 08:41:25 -0700

    Adding config to satisfy SD element 146

--- src/main/java/mil/af/abms/midas/config/AppConfig.java
+++ src/main/java/mil/af/abms/midas/config/AppConfig.java
@@ -9,10 +9,12 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -47,6 +49,17 @@ public class AppConfig implements WebMvcConfigurer {
             }
             flyway.migrate();
         };
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.
+                favorParameter(true).
+                parameterName("mediaType").
+                ignoreAcceptHeader(true).
+                useRegisteredExtensionsOnly(false).
+                defaultContentType(MediaType.APPLICATION_JSON).
+                mediaType("json", MediaType.APPLICATION_JSON);
     }
 
 }
