a400f0ac09de503bf6d41d5002822d155cbdab2f
Danny Benson <dbenson@rise8.us>
2021-12-23 09:04:36 -0500

    Upgrade api to springboot 2.6.2

--- localRun/experimental/mysql/init-db.sql
+++ localRun/experimental/mysql/init-db.sql
@@ -1,18 +1,18 @@
-DROP DATABASE IF EXISTS appDB;
-DROP DATABASE IF EXISTS appDB_test;
+DROP DATABASE IF EXISTS midas_db;
+DROP DATABASE IF EXISTS midas_db_test;
 
 DROP USER IF EXISTS localDBUser;
 DROP USER IF EXISTS local_test;
 
-CREATE DATABASE appDB;
-CREATE DATABASE appDB_test;
+CREATE DATABASE midas_db;
+CREATE DATABASE midas_db_test;
 
-CREATE USER 'localDBUser'@'%' IDENTIFIED BY 'localDBUser';
+CREATE USER 'localDBUser'@'%' IDENTIFIED BY 'localDBPassword';
 CREATE USER 'local_test'@'%' IDENTIFIED BY 'local_test';
 CREATE USER IF NOT EXISTS 'root'@'%';
 
-GRANT ALL ON appDB.* TO 'localDBUser'@'%' WITH GRANT OPTION;
-GRANT ALL ON appDB_test.* TO 'local_test'@'%' WITH GRANT OPTION;
+GRANT ALL ON midas_db.* TO 'localDBUser'@'%' WITH GRANT OPTION;
+GRANT ALL ON midas_db_test.* TO 'local_test'@'%' WITH GRANT OPTION;
 GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;
 
 SET GLOBAL time_zone = "+00:00";
