a83df8e3b2059ec537c25110a3c8f82ac5424fc2
Danny Benson <dbenson@rise8.us>
2022-08-03 18:56:12 +0000

    E2E testing

---
+++ cypress/integration/test.spec.cy.js
@@ -0,0 +1,8 @@
+/*
+ * Measures, Integration, Deployment, & Analytics System (MIDAS) End-to-end Tests
+ */
+describe('Pass MIDAS e2e_tests', () => {
+    it('is working', () => {
+        expect(true).to.equal(true)
+    })
+})
