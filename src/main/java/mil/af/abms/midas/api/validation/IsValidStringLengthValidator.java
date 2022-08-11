a8507ed08351fe3f15b5e6e680e3ff3d807e00bc
Danny Benson <dbenson@rise8.us>
2022-08-11 14:41:57 -0400

    Update string length validator

--- src/main/java/mil/af/abms/midas/api/validation/IsValidStringLengthValidator.java
+++ src/main/java/mil/af/abms/midas/api/validation/IsValidStringLengthValidator.java
@@ -16,6 +16,7 @@ public class IsValidStringLengthValidator implements ConstraintValidator<IsValid
     @Override
     public boolean isValid(String string, ConstraintValidatorContext constraintContext) {
         constraintContext.buildConstraintViolationWithTemplate("Max characters: " + maxLength).addConstraintViolation();
-        return string.length() <= maxLength;
+
+        return string == null || string.length() <= maxLength;
     }
 }
